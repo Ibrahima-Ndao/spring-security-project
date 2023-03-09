6e32043365c34f4f30fff0baa964ef0fe6ac0f77
Ibrahima-Ndao <ibrahimandao394@gmail.com>
2023-03-09 23:56:47 +0000

    User and role enum  Added

---
+++ spring-boot-security-book-seller-project/src/main/java/com/spring/security/model/User.java
@@ -0,0 +1,39 @@
+package com.spring.security.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "username", unique = true, nullable = false)
+	private String username;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	@Column(name = "CreateTime", nullable = false)
+	private LocalDate createTime;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "Role", nullable = false)
+	private Role role;
+}
